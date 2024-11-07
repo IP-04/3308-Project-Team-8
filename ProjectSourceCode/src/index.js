3fdf10a388e6c3c5dff5d86a6a345e0d0e749662
Ryan O'Leary <ryol7118@colorado.edu>
2024-11-07 14:15:05 -0700

    Updated register API
    now has 200 status and send 'success' message

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -124,6 +124,7 @@ app.post('/register', async (req, res) => {
   const hashedPassword = bcrypt.hashSync(password, 10);
   try {
     await db.none('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
+    res.stats(200).send('Success');
     res.redirect('/login');
   } catch (error) {
     res.status(500).send('Error creating user');
