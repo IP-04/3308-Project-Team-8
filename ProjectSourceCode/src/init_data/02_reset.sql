f57cf5e5dc996f8522a4b811f355bf5e4e5b4e64
Zach Dyre <zkdyre@gmail.com>
2024-11-21 16:03:49 -0700

    cloud now completely working, db resettable

--- ProjectSourceCode/src/init_data/02_reset.sql
+++ ProjectSourceCode/src/init_data/02_reset.sql
@@ -1,4 +1,11 @@
-/* // ONLY UNCOMMENT IF TRYING TO CLEAR OUT THE DATABASE (run 'sh init_db.sh' before and after uncommenting)
+/* ONLY UNCOMMENT IF TRYING TO CLEAR OUT THE DATABASE 
+ * run 'sh init_db.sh' after uncommenting
+ * in Render, manually deploy web service selecting 'Clear build cache and deploy'
+ * re comment out this file
+ * run 'sh init_db.sh' again
+ */
+ 
+/*
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS profiles;
 DROP TABLE IF EXISTS books;
