48887c4ad8c11ce842aadc9313bc14b1b4053457
Zach Dyre <zkdyre@gmail.com>
2024-11-21 15:34:49 -0700

    added db reset file

---
+++ ProjectSourceCode/src/init_data/02_reset.sql
@@ -0,0 +1,11 @@
+/* // ONLY UNCOMMENT IF TRYING TO CLEAR OUT THE DATABASE (run 'sh init_db.sh' before and after uncommenting)
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS profiles;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS users_to_profiles;
+DROP TABLE IF EXISTS reviews_to_books;
+DROP TABLE IF EXISTS profiles_to_books;
+DROP TABLE IF EXISTS reviews_to_profiles;
+*/
